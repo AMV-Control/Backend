75f23d0ee74f6ff00bb0419f05a9967385254535
Алексей <alexleskov2000@gmail.com>
2022-08-30 13:35:57 +0800

    Изменить кавычки

--- app/core/config.py
+++ app/core/config.py
@@ -7,10 +7,10 @@ class Settings(BaseSettings):
     PROJECT_NAME: str
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
-    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    @validator('BACKEND_CORS_ORIGINS', pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
-        if isinstance(v, str) and not v.startswith("["):
-            return [i.strip() for i in v.split(",")]
+        if isinstance(v, str) and not v.startswith('['):
+            return [i.strip() for i in v.split(',')]
         elif isinstance(v, (list, str)):
             return v
         raise ValueError(v)
@@ -22,7 +22,7 @@ class Settings(BaseSettings):
 
     class Config:
         case_sensitive = True
-        env_file = ".env"
+        env_file = '.env'
 
 
 settings = Settings()
